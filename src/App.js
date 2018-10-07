259ce88e95244173c1f4f1ad1b5f74f469927d0f
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-10-07 20:43:58 -0300

    limpieza de componentes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Exp from './Components/Fetch'
 
@@ -7,11 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+        <header>
+         <h1>Pinterest - react</h1>
         </header>
         <Exp />
       </div>
