7702be7d6f59efec834ccc52d575e72236bee3eb
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-10-07 18:38:02 -0300

    Updates

---
+++ precache-manifest.4618ff1c18b743b5021fced83fc8db38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/svalley/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9e6979fecb3f0a982c0f",
+    "url": "/svalley/static/js/runtime~main.9e6979fe.js"
+  },
+  {
+    "revision": "4ae34cbf5e760d0ce79b",
+    "url": "/svalley/static/js/main.4ae34cbf.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/svalley/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "4ae34cbf5e760d0ce79b",
+    "url": "/svalley/static/css/main.e8127bc4.chunk.css"
+  },
+  {
+    "revision": "54c07376a45b7c36c1a804a04238377a",
+    "url": "/svalley/index.html"
+  }
+];
