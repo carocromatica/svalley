8a4d12aada4eb4491bb693caec4d08a64d9ba8d2
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-10-08 03:33:52 -0300

    carga pixabay funciona

--- src/Components/Pins.js
+++ src/Components/Pins.js
@@ -1,37 +1,41 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class LoadPins extends Component {
 
+class LoadPins extends Component {
+  
   render() {
     let imageListContent;
-    const { pins } = this.props;
-    console.log(pins)
+    const { images } = this.props;
 
-    if (pins) {
+    if (images) {
       imageListContent = (
         <div>
-          {pins.map(img => (
+          {images.map(img => (
             <div>
+           
               <img src={img.largeImageURL} alt="" />
-              carga
             </div>
           ))}
         </div>
-      )
+      );
     } else {
       imageListContent = null;
+    }
 
-    } return (
+
+    return (
       <div>
         {imageListContent}
+     
+
       </div>
-    )
+    );
   }
 }
 
 LoadPins.propTypes = {
-  pins: PropTypes.array.isRequired
+  images: PropTypes.array.isRequired
 };
 
 export default LoadPins;
--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -5,42 +5,52 @@ import LoadPins from './Pins'
 
 class SearchBar extends Component {
   
+  state = {
+    searchText: '',
+    amount:20,
+    apiUrl:'https://pixabay.com/api/',
+    apiKey:'1721901-fa9bcc6ed3f879da0567a53bb',
+    images:[]
+  };
 
-state ={
-  inputText:'',
-  amount:20,
-  apiUrl:'https://pixabay.com/api/',
-  apiKey:'1721901-fa9bcc6ed3f879da0567a53bb',
-  pins:[]
-};
+  onTextChange = e => {
+    const val = e.target.value;
+    this.setState({ [e.target.name]: val }, () => {
+      if (val === '') {
+        this.setState({ images: [] });
+      } else {
+        axios
+          .get(
+            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+              this.state.searchText
+            }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+          )
+          .then(res => this.setState({ images: res.data.hits }))
+          .catch(err => console.log(err));
+      }
+    });
+  };
 
-onTextChange = e =>{
-  this.setState({[e.target.name]:e.target.value},()=>
-{
-  axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.inputText}&image_type=photo&per_page=${this.state.amount}$safesearch=true`)
-  .then(resp=>this.setState({pins:resp.data.hits}))
-  .catch(err=> console.log('err'))
-
-});
-};
+  onAmountChange = (e, index, value) => this.setState({ amount: value });
 
   render() {
-    console.log(this.state.pins)
+    console.log(this.state.images);
     return (
-      
       <div>
-        
-        <input name="inputText" value={this.state.inputText} 
-        onChange={this.onTextChange} 
-        
-        type="text" placeholder="Búsqueda.."/>
-      
-         
-    {this.state.pins.lenght > 0 ? (<LoadPins pins={this.state.pins} />) : null}
+        <input
+          name="searchText"
+          value={this.state.searchText}
+          onChange={this.onTextChange}
+    
+        />
+
+        {this.state.images.length > 0 ? (
+          <LoadPins images={this.state.images} />
+        ) : null}
       </div>
-     
     );
   }
+
 }
 
 export default SearchBar;
