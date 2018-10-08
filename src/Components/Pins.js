dd069f7bda9b6e8bf343ff53825960b4bb16daa1
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-10-08 05:22:17 -0300

    carga de imagenes al principio

--- src/Components/Pins.js
+++ src/Components/Pins.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class LoadPins extends Component {
-  
+
   render() {
     let imageListContent;
     const { pins } = this.props;
@@ -11,12 +11,11 @@ class LoadPins extends Component {
       imageListContent = (
         <div className="row">
           {pins.map(img => (
-
             <div className="column">
-              <img src={img.webformatURL} alt="" />
-              <p>{img.tags}</p>
+              <a href="#popup" class="popup-link"> <img src={img.webformatURL} alt="" />
+              </a><p>{img.tags}</p>
             </div>
-           
+
           ))}
         </div>
       );
@@ -25,7 +24,16 @@ class LoadPins extends Component {
     }
     return (
       <div>
-        {imageListContent}
+        <div>
+          {imageListContent}
+        </div>
+        <div class="modal-wrapper" id="popup">
+          <div class="popup-contenedor">
+            <h2>Titulo de la Modal</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor deleniti...</p>
+            <a class="popup-cerrar" href="#">X</a>
+          </div>
+        </div>
       </div>
     );
   }
--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -3,26 +3,32 @@ import axios from 'axios'
 import LoadPins from './Pins'
 
 class SearchBar extends Component {
-  
+
   state = {
     inputText: '',
-    amount:200,
-    apiUrl:'https://pixabay.com/api/',
-    apiKey:'1721901-fa9bcc6ed3f879da0567a53bb',
-    pins:[]
+    amount: 200,
+    apiUrl: 'https://pixabay.com/api/',
+    apiKey: '1721901-fa9bcc6ed3f879da0567a53bb',
+    pins: []
   };
 
-  
+  componentDidMount() {
+    axios
+    .get(
+      `${this.state.apiUrl}/?key=${this.state.apiKey}&q=watercolor&orientation=vertical&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
+    .then(res => this.setState({ pins: res.data.hits }))
+    .catch(err => console.log(err));
+  }
 
   onTextChange = e => {
     const input = e.target.value;
     this.setState({ [e.target.name]: input }, () => {
       if (input === '') {
         axios
-        .get(
-          `${this.state.apiUrl}/?key=${this.state.apiKey}&q=flower&orientation=vertical&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
-        .then(res => this.setState({ pins: res.data.hits }))
-        .catch(err => console.log(err));
+          .get(
+            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=watercolor&orientation=vertical&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
+          .then(res => this.setState({ pins: res.data.hits }))
+          .catch(err => console.log(err));
       } else {
         axios
           .get(
@@ -38,8 +44,8 @@ class SearchBar extends Component {
     return (
       <div>
         <input
-          name="inputText" value={this.state.inputText} onChange={this.onTextChange}/>
-         <LoadPins pins={this.state.pins} /> 
+          name="inputText" value={this.state.inputText} onChange={this.onTextChange} />
+        <LoadPins pins={this.state.pins} />
       </div>
     );
   }
