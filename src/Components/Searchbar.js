3ab22959eb731c8accdfbe94ed545994cdccc891
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-10-08 00:44:28 -0300

    search + load api: OK

--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 
 class SearchBar extends Component {
+  
 
 state ={
   inputText:'',
-  amount:15,
+  amount:20,
   apiUrl:'https://pixabay.com/api/',
   apiKey:'1721901-fa9bcc6ed3f879da0567a53bb',
   pins:[]
-}
+};
+
+onTextChange = e =>{
+  this.setState({[e.target.name]:e.target.value},()=>
+{
+  axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.inputText}&image_type=photo&per_page=${this.state.amount}$safesearch=true`)
+  .then(resp=>this.setState({pins:resp.data.hits}))
+  .catch(err=> console.log('err'))
+
+});
+};
 
   render() {
+    console.log(this.state.pins)
     return (
+      
       <div>
         
         <input name="inputText" value={this.state.inputText} 
