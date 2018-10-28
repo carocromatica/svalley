1be9d35eb41583acee5c13eaa5c6819b2975b1ef
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-10-27 21:36:31 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/svalley/precache-manifest.522c070802b439bc13044eab1dee2cd2.js"
+  "/svalley/precache-manifest.7a0f5095fc8e096631c40b5f471b50a1.js"
 );
 
 workbox.clientsClaim();
